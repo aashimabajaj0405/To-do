e9b4b9a9f33ea49497675c2b854815eef7f039e8
aashimabajaj0405 <aashimabajaj2@gmail.com>
2020-08-05 16:24:54 -0700

    make some changes

--- index.js
+++ index.js
@@ -11,14 +11,22 @@ var list = document.getElementById("myList");
 function getFile() {
   for (let i = 0; i < itemArray.length; i++) {
     var text = itemArray[i].task;
-    var listItem = document.createElement("li"); 
-    var textNode = document.createTextNode(text); 
-    listItem.setAttribute(
-      "ondblclick",
-      "this.style.textDecoration = 'line-through'"
-    );
-    listItem.appendChild(textNode); 
-    list.appendChild(listItem); 
+    var listItem = document.createElement("li");
+    var textNode = document.createTextNode(text);
+    if (itemArray[i].completed == "false") {
+      listItem.setAttribute(
+        "ondblclick",
+        "this.style.textDecoration = 'line-through'"
+      );
+    } 
+    else {
+      listItem.setAttribute(
+        "style", 
+        "text-decoration: line-through"
+      );
+    }
+    listItem.appendChild(textNode);
+    list.appendChild(listItem);
     textBox.value = "";
   }
 }
@@ -26,18 +34,18 @@ function getFile() {
 //To add new file
 function addFile() {
   var text = textBox.value;
-    var listItem = document.createElement("li"); 
-    var textNode = document.createTextNode(text); 
-    listItem.setAttribute(
-      "ondblclick",
-      "this.style.textDecoration = 'line-through'"
-    );
-    listItem.appendChild(textNode); 
-    list.appendChild(listItem); 
-    textBox.value = "";
+  var listItem = document.createElement("li");
+  var textNode = document.createTextNode(text);
+  listItem.setAttribute(
+    "ondblclick",
+    "this.style.textDecoration = 'line-through'"
+  );
+  listItem.appendChild(textNode);
+  list.appendChild(listItem);
+  textBox.value = "";
 }
 
-//To clear file
+//To clear completed file
 function clearFile() {
   var clearText = document.querySelectorAll("li");
   clearText.forEach((a) => {
@@ -67,26 +75,32 @@ if (itemArray == null) {
   itemArray = [];
 }
 function saveFile() {
-  itemArray=[];
+  itemArray = [];
+  var completed;
   for (let i = 0; i < list.childNodes.length; i++) {
+    if (list.childNodes[i].style.textDecoration == "line-through") {
+      completed = "true";
+    } 
+    else {
+      completed = "false";
+    }
     var file = {
       task: list.childNodes[i].textContent,
+      completed,
     };
     itemArray.push(file);
   }
-  if(itemArray.length>0)
-   localStorage.setItem("file", JSON.stringify(itemArray));
+  if (itemArray.length > 0)
+    localStorage.setItem("file", JSON.stringify(itemArray));
 }
 
 //Event Listeners
 document.onload = getFile();
-textBox.addEventListener("keypress",function(event){
-   if(textBox.value!==''&&event.which=='13') 
-      addFile();
+textBox.addEventListener("keypress", function (event) {
+  if (textBox.value !== "" && event.which == "13") addFile();
 });
-addButton.addEventListener("click", function(){
-   if(textBox.value!=='') 
-      addFile();
+addButton.addEventListener("click", function () {
+  if (textBox.value !== "") addFile();
 });
 clearButton.addEventListener("click", clearFile);
 emptyButton.addEventListener("click", emptyFile);
